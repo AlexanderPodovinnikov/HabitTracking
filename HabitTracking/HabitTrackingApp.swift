0db715631fe2b69170374094053915b41a444408
AlexanderPodovinnikov <art.iq@hotmail.com>
2022-05-26 14:17:58 +0300

    Initial Commit

---
+++ HabitTracking/HabitTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackingApp.swift
+//  HabitTracking
+//
+//  Created by Alex Po on 26.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
